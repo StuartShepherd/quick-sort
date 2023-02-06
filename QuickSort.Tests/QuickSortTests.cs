6aba1ecdf8c9c9da34d9c4056f340ca84e9cf7b3
Stuart Shepherd <stuart@createstudios.com.au>
2023-02-07 08:19:02 +1100

    Add project files.

---
+++ QuickSort.Tests/QuickSortTests.cs
@@ -0,0 +1,20 @@
+namespace QuickSort.Tests;
+
+[TestClass]
+public class QuickSortTests
+{
+    [TestClass]
+    public class MergeSortTests
+    {
+        [TestMethod]
+        [DataRow(new int[] { 73, 57, 49, 99, 133, 20, 1 }, new int[] { 1, 20, 49, 57, 73, 99, 133 })]
+        public void Sorts_Array_ReturnsSortedArray(int[] x, int[] expected)
+        {
+            var quickSort = new QuickSort();
+            var actual = quickSort.Sort(x);
+
+            Assert.IsNotNull(actual);
+            CollectionAssert.AreEqual(actual, expected);
+        }
+    }
+}
---
+++ QuickSort/QuickSort.cs
@@ -0,0 +1,48 @@
+﻿namespace QuickSort;
+
+public class QuickSort
+{
+    public int[] Sort(int[] array)
+    {
+        return  SortArray(array, 0, array.Length - 1);
+    }
+
+    private int[] SortArray(
+        int[] array, 
+        int leftIndex, 
+        int rightIndex)
+    {
+        var i = leftIndex;
+        var j = rightIndex;
+        var pivot = array[leftIndex];
+
+        while (i <= j)
+        {
+            while (array[i] < pivot)
+            {
+                i++;
+            }
+
+            while (array[j] > pivot)
+            {
+                j--;
+            }
+            if (i <= j)
+            {
+                int temp = array[i];
+                array[i] = array[j];
+                array[j] = temp;
+                i++;
+                j--;
+            }
+        }
+
+        if (leftIndex < j)
+            SortArray(array, leftIndex, j);
+
+        if (i < rightIndex)
+            SortArray(array, i, rightIndex);
+
+        return array;
+    }
+}
