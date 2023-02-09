a7715799882b8df130c88cb88e5f43038587f914
Stuart Shepherd <stuart@createstudios.com.au>
2023-02-09 16:54:24 +1100

    Fixed the tests class.

--- QuickSort.Tests/QuickSortTests.cs
+++ QuickSort.Tests/QuickSortTests.cs
@@ -1,20 +1,15 @@
 namespace QuickSort.Tests;
 
-[TestClass]
 public class QuickSortTests
 {
-    [TestClass]
-    public class MergeSortTests
+    [TestMethod]
+    [DataRow(new int[] { 73, 57, 49, 99, 133, 20, 1 }, new int[] { 1, 20, 49, 57, 73, 99, 133 })]
+    public void Sorts_Array_ReturnsSortedArray(int[] x, int[] expected)
     {
-        [TestMethod]
-        [DataRow(new int[] { 73, 57, 49, 99, 133, 20, 1 }, new int[] { 1, 20, 49, 57, 73, 99, 133 })]
-        public void Sorts_Array_ReturnsSortedArray(int[] x, int[] expected)
-        {
-            var quickSort = new QuickSort();
-            var actual = quickSort.Sort(x);
+        var quickSort = new QuickSort();
+        var actual = quickSort.Sort(x);
 
-            Assert.IsNotNull(actual);
-            CollectionAssert.AreEqual(actual, expected);
-        }
+        Assert.IsNotNull(actual);
+        CollectionAssert.AreEqual(actual, expected);
     }
 }
